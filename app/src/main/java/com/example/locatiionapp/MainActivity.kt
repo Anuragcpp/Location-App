a4e8127b741ee595fb3d44d271f33c8309c4c650
Anurag Sen <anurag007sen@gmail.com>
2024-07-27 18:31:16 +0530

    workin with the permission

--- app/src/main/java/com/example/locatiionapp/MainActivity.kt
+++ app/src/main/java/com/example/locatiionapp/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.locatiionapp
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.locatiionapp.ui.theme.LocatiionAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +31,79 @@ class MainActivity : ComponentActivity() {
         setContent {
             LocatiionAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LocatiionAppTheme {
-        Greeting("Android")
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    context: Context
+) {
+
+    val requestPermissionLoncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions() ,
+        onResult = {
+            permissions ->
+            if(permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+                && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true){
+                // I have access to locatoin
+            }else{
+                // ask for permission
+                val retionaleRequired = ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) || ActivityCompat.shouldShowRequestPermissionRationale(
+                    context as MainActivity,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+                
+                if (retionaleRequired){
+                    Toast.makeText(
+                        context,
+                        "Location permission is required for this feature to work",
+                        Toast.LENGTH_SHORT
+                        ).show()
+                }else{
+                    Toast.makeText(
+                        context,
+                        "Location permission is required please enabled it in the android settings",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    )
+
+    Column (modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+        ) {
+
+        Text(text = "Location not Available")
+        
+        Button(onClick = {
+            if(locationUtils.hasLocationPermission(context)){
+                //permission already granted update the location
+            }else{
+                // request for the permission
+                requestPermissionLoncher.launch(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                )
+            }
+        }) {
+
+            Text(text ="Get Location")
+            
+        }
+
     }
+
 }
